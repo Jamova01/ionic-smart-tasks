686b2313a162de291e1a5daf979aa4b3d615ffbc
Jamova01 <jorgemova01@gmail.com>
2026-01-09 12:03:09 -0500

    chore(app): Import IonicModule in standalone App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [IonicModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('ionic-smart-tasks');
-}
+export class App {}
