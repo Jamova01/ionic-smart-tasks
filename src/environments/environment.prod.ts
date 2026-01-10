683bf49338cc195d4da07cc657f7f4cabc6d14ba
Jamova01 <jorgemova01@gmail.com>
2026-01-10 08:20:05 -0500

    chore(env): add firebase configuration for dev and prod environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAMHp7dJXQeafHGjLI_f1zEEchUZ1uuAu4',
+    authDomain: 'ionicsmarttasks.firebaseapp.com',
+    projectId: 'ionicsmarttasks',
+    storageBucket: 'ionicsmarttasks.firebasestorage.app',
+    messagingSenderId: '130135162798',
+    appId: '1:130135162798:web:ad503e2f78e0f45248d23f',
+    measurementId: 'G-MP82L6H7ZV',
+  },
+};
